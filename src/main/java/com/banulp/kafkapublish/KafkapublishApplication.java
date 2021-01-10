65f01344b917440ddafeadd11aa5f9037e2430bb
banulp <banulp@gmail.com>
2021-01-10 13:00:24 +0900

    first commit

---
+++ src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
@@ -0,0 +1,13 @@
+package com.banulp.kafkapublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkapublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkapublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banulp/kafkapublish/KafkapublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banulp.kafkapublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkapublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
