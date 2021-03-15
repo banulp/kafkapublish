a4168d4c905970ce895d977bd9fde987ae8ba078
banulp <banulp@gmail.com>
2021-03-16 03:35:16 +0900

    sm

--- README.md
+++ README.md
@@ -19,3 +19,9 @@ https://www.daangn.com/images?image_index=0&object_type=Article&object_id=206800
 {"id":"206800996","title":"노트북","region":"성남시 분당구 판교동"}
 {"id":"206908580","title":"와플 팬","region":"성남시 분당구 운중동"}
 {"id":"206721398","title":"레고블록","region":"성남시 중원구 성남동"}
+
+#!/bin/bash
+mvn clean package -Dmaven.test.skip=true
+
+#!/bin/bash
+java -jar -Dspring.profiles.active=ubuntu /home/banulp/kafka/pub/kafkapublish/kafkapublish-0.0.1-SNAPSHOT.jar
--- src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
+++ src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
@@ -23,12 +23,13 @@ public class KafkapublishApplication {
     @Bean
     public ApplicationRunner applicationRunner() {
         return args -> {
-
+            System.out.println("start");
             List<String> list = new ArrayList<String>();
             list.add("{\"id\":\"206800996\",\"title\":\"노트북\",\"region\":\"성남시 분당구 판교동\"}");
             list.add("{\"id\":\"206908580\",\"title\":\"와플 팬\",\"region\":\"성남시 분당구 운중동\"}");
             list.add("{\"id\":\"206721398\",\"title\":\"레고블록\",\"region\":\"성남시 중원구 성남동\"}");
             list.forEach(l -> {
+                System.out.println("in");
                         ksm.sendMessage(l);
                     }
             );
