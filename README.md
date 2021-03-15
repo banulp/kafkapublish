d186049d11b83ca37910cc5ded2e47caf38b7c2f
banulp <banulp@gmail.com>
2021-03-16 02:48:05 +0900

    fm

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # kafkapublish
+
+https://www.daangn.com/articles/
+
+0.
+https://www.daangn.com/images?image_index=0&object_type=Article&object_id=206800996
+
+1.
+<p id="article-price-nanum" property="schema:price" content="0.0" style="font-size:18px; font-weight:bold;">
+            무료나눔
+        </p>
+
+2.
+<h1 property="schema:name" id="article-title" style="margin-top:0px;">미사용  노트북 ㆍ서류가방드림</h1>
+
+3.
+<div id="region-name">성남시 분당구 판교동</div>
+
+{"id":"206800996","title":"노트북","region":"성남시 분당구 판교동"}
+{"id":"206908580","title":"와플 팬","region":"성남시 분당구 운중동"}
+{"id":"206721398","title":"레고블록","region":"성남시 중원구 성남동"}
--- src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
+++ src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
@@ -1,13 +1,37 @@
 package com.banulp.kafkapublish;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class KafkapublishApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkapublishApplication.class, args);
-	}
+    @Autowired
+    private KafkaSendMessage ksm;
 
+    public static void main(String[] args) {
+        SpringApplication.run(KafkapublishApplication.class, args);
+    }
+
+    @Bean
+    public ApplicationRunner applicationRunner() {
+        return args -> {
+
+            List<String> list = new ArrayList<String>();
+            list.add("{\"id\":\"206800996\",\"title\":\"노트북\",\"region\":\"성남시 분당구 판교동\"}");
+            list.add("{\"id\":\"206908580\",\"title\":\"와플 팬\",\"region\":\"성남시 분당구 운중동\"}");
+            list.add("{\"id\":\"206721398\",\"title\":\"레고블록\",\"region\":\"성남시 중원구 성남동\"}");
+            list.forEach(l -> {
+                        ksm.sendMessage(l);
+                    }
+            );
+        };
+    }
 }
