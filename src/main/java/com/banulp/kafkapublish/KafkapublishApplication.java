7233b1ecf1f2250c997e5e8151bf6e5f13a5a6a8
banulp <banulp@gmail.com>
2021-03-23 02:33:22 +0900

    params

--- src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
+++ src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
@@ -24,18 +24,21 @@ public class KafkapublishApplication {
     public ApplicationRunner applicationRunner() {
         return args -> {
             System.out.println("start");
+
+            Arrays.asList(args).forEach(a -> System.out.println(a));
+
             List<String> list = new ArrayList<String>();
             list.add("{\"id\":\"206800996\",\"title\":\"노트북\",\"region\":\"성남시 분당구 판교동\"}");
             list.add("{\"id\":\"206908580\",\"title\":\"와플 팬\",\"region\":\"성남시 분당구 운중동\"}");
             list.add("{\"id\":\"206721398\",\"title\":\"레고블록\",\"region\":\"성남시 중원구 성남동\"}");
-//            list.forEach(l -> {
-//                System.out.println("in");
-//                        ksm.sendMessage(l);
-//                    }
-//            );
-            for (int i = 0; i < 10; i++) {
-                ksm.sendMessage("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
-            }
+            list.forEach(l -> {
+                System.out.println("in");
+                        ksm.sendMessage(l);
+                    }
+            );
+//            for (int i = 0; i < 10; i++) {
+//                ksm.sendMessage("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+//            }
         };
     }
 }
