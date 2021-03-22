e83e9e04a70258b97a68632098c7eb2628a75e81
banulp <banulp@gmail.com>
2021-03-23 02:50:18 +0900

    kafka

--- src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
+++ src/main/java/com/banulp/kafkapublish/KafkapublishApplication.java
@@ -24,17 +24,20 @@ public class KafkapublishApplication {
     public ApplicationRunner applicationRunner() {
         return args -> {
             System.out.println("[[start]]");
-            Arrays.asList(args.getSourceArgs()).forEach(a -> System.out.println(a));
+            Arrays.asList(args.getSourceArgs()).forEach(a -> {
+//                System.out.println(a)
+                ksm.sendMessage(a);
+            });
 
-            List<String> list = new ArrayList<String>();
-            list.add("{\"id\":\"206800996\",\"title\":\"노트북\",\"region\":\"성남시 분당구 판교동\"}");
-            list.add("{\"id\":\"206908580\",\"title\":\"와플 팬\",\"region\":\"성남시 분당구 운중동\"}");
-            list.add("{\"id\":\"206721398\",\"title\":\"레고블록\",\"region\":\"성남시 중원구 성남동\"}");
-            list.forEach(l -> {
-                System.out.println("in");
-                        ksm.sendMessage(l);
-                    }
-            );
+//            List<String> list = new ArrayList<String>();
+//            list.add("{\"id\":\"206800996\",\"title\":\"노트북\",\"region\":\"성남시 분당구 판교동\"}");
+//            list.add("{\"id\":\"206908580\",\"title\":\"와플 팬\",\"region\":\"성남시 분당구 운중동\"}");
+//            list.add("{\"id\":\"206721398\",\"title\":\"레고블록\",\"region\":\"성남시 중원구 성남동\"}");
+//            list.forEach(l -> {
+//                System.out.println("in");
+//                        ksm.sendMessage(l);
+//                    }
+//            );
 //            for (int i = 0; i < 10; i++) {
 //                ksm.sendMessage("kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
 //            }
